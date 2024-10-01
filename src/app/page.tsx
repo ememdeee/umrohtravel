5d5d7b798d8b55bf3e9d48b4bfa6c06471d23b01
ememdeee <basurah.muhammad.mb@gmail.com>
2024-10-01 11:13:33 +0700

    section 1,2,3 i think done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
-import { Button } from '@/components/ui/button'
 import TravelSlider from '@/components/TravelSlider'
 import LuxuryTravelExperts from '@/components/luxury-travel-experts'
 import DestinationShowcase from '@/components/destination-showcase'
